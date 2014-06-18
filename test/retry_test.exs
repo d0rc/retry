03d36e7f3f0e06db8505660bf419a2d4c98f0b22
d0rc <7993863@gmail.com>
2014-06-18 23:18:15 +0400

    test

--- test/retry_test.exs
+++ test/retry_test.exs
@@ -1,7 +1,8 @@
 defmodule RetryTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it works" do
+    result = Retry.run fn -> :ets.insert :hello, {2,1} end, %{sleep: 0, tries: 1000}
+  	assert %{attempts: 1000, state: :failed} = result
   end
 end
